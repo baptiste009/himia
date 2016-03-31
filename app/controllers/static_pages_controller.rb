0c25e8e2fbb2dcc5dd9ad0f8769e231f8d78e194
baptiste009 <timseithe@googlemail.com>
2016-03-31 19:50:05 +0200

    Email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
+
   def index
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'timseithe@googlemail.com',
+      :subject => "Neue Nachricht von #{@name}",
+      :body => @message).deliver_now
+end
+
 end
